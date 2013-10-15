693e605fc4e597f58cad3099b4e66d3cba45d2fc
Compte etudiant <etudiant@LKQ.localdomain>
2013-10-15 11:43:46 +0200

    chainage arrière unfinished version

---
+++ src/ChainageArriere.java
@@ -0,0 +1,51 @@
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class ChainageArriere {
+	
+	public boolean demo(Fact goal, List<Fact> facts, List<Rule> rules){
+		boolean dem = false;
+		//list demandable
+		List<Fact> demandables = new LinkedList<Fact>();
+		
+		// initialization of demandables
+		//TODO
+		
+		// first case
+		if (facts.contains(goal))
+			dem = true;
+		
+		// second case
+		for (Rule r : rules){
+			if (r.getDeducedFacts() == goal){
+				while(!dem){	
+					dem = verif(r.getRequiredFacts(),facts, rules);
+				}
+			}
+		}
+		
+		// third case
+			
+		if(!dem && demandables.contains(goal)){
+						//TODO
+		}
+		
+		if(dem){
+			facts.add(goal);
+		}
+		
+		return dem;
+	}
+	
+	public boolean verif(List<Fact> goals, List<Fact> bf, List<Rule> rules){
+		boolean ver = true;
+		for(Fact f : goals){
+			while(ver){
+				ver = demo(f, bf, rules);
+			}
+		}
+		return ver;
+	}
+
+}
