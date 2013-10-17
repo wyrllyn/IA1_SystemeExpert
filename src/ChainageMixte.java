d84f87c87b7e921f58a7cd56847e2eaab4f57a2f
Compte etudiant <etudiant@LKQ.localdomain>
2013-10-17 16:14:56 +0200

    prototype ChainageMixte

---
+++ src/ChainageMixte.java
@@ -0,0 +1,76 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class ChainageMixte {
+	
+	/*
+	 TREE
+: F (à déduire)
+DEBUT
+TANT QUE
+F n'est pas déduit mais peut encore l'être
+FAIRE
+- Saturer la base de faits par chaînage AVANT
+(c'est-à-dire, déduire tout ce qui peut être
+déduit)
+- Chercher quels sont les faits encore déductibles
+- Déterminer une question pertinente à poser à
+l'utilisateur
+et ajouter sa réponse à la base de faits
+FIN DU TANT QUE
+FIN
+	 */
+	public ChainageAvant CA;
+	
+	void procedure(List<Rule> rules, List<Fact> facts, Fact goal, Set<Fact> possibleFacts){
+		boolean dem = false;
+		boolean deductible = true;
+		Scanner scanner = new Scanner (System.in);
+		
+		//list demandable
+		List<Fact> demandables = new LinkedList<Fact>();
+		
+		// initialization of demandables
+		for (Fact possible : possibleFacts){
+			boolean present = false;
+			for (Rule r : rules){
+				if (possible == r.getDeducedFact())
+					present = true;
+			}
+			if (!present)
+				demandables.add(possible);
+		}
+		
+		if (facts.contains(goal)) {
+			System.out.println("goal is already in base facts");
+			dem = true;
+		}
+		
+		while(!dem){		
+				CA.procedure(rules, facts);
+			
+			if (facts.contains(goal)) {
+				System.out.println("goal is already in base facts");
+				break;
+			}
+			
+			if(!dem && demandables.contains(goal)){
+				System.out.println("add "+goal.getName()+ " in the bf ? 1 for yes, anything for no");
+				int val = scanner.nextInt ();
+				if(val == 1)
+					dem = true;
+				else dem = false;
+			}
+			
+			if(dem){
+				System.out.println(" dem = true");
+				facts.add(goal);
+			}
+		}
+	}	
+}
+
+
